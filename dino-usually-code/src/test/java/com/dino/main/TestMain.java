e66d0e64dbb4852a4e7dca8f947b856767a0dd9e
licl <licl@caibab.com>
2019-02-15 09:09:04 +0800

    TestMain提交

---
+++ dino-usually-code/src/test/java/com/dino/main/TestMain.java
@@ -0,0 +1,19 @@
+package com.dino.main;
+
+public class TestMain {
+	public static void main(String[] args) {
+		System.out.println(longestPalindrome("abcdcbaefgxxt"));
+	}
+	
+	public static String longestPalindrome(String s) {
+		
+		int i = 0;
+		int j = 0;
+		
+		while(i<s.length()&&j<s.length()){
+			
+		}
+		
+        return "";
+    }
+}
